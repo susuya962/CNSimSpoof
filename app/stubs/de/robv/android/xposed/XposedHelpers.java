7a6dd8269e67f94afe8ac9d3eeb8fde7b375ec5f
susuya962 <hgh20090714@gmail.com>
2026-08-20 05:01:55 +0800

    add app/stubs/de/robv/android/xposed/XposedHelpers.java

---
+++ app/stubs/de/robv/android/xposed/XposedHelpers.java
@@ -0,0 +1,16 @@
+package de.robv.android.xposed;
+
+public final class XposedHelpers {
+    public static XC_MethodHook.Unhook findAndHookMethod(Class<?> clazz, String methodName,
+            Object... parameterTypesAndCallback) {
+        return null;
+    }
+
+    public static Class<?> findClass(String className, ClassLoader classLoader) {
+        return null;
+    }
+
+    public static Class<?> findClassIfExists(String className, ClassLoader classLoader) {
+        return null;
+    }
+}
